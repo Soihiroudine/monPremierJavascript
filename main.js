5c22e0854131d2f52c5d6b5ac966b8243404bbc6
Soihiroudine <59316622+Soihiroudine@users.noreply.github.com>
2024-10-09 15:40:47 +0300

    Apprentissage commentaire, variable et la concatenation

---
+++ main.js
@@ -0,0 +1,33 @@
+// ---- LES COMMENTAIRES ----
+
+// Commentaire sur une ligne
+
+/*
+    Commentaire sur plusieurs ligne
+*/
+
+// ---- LES VARIABLES ----
+
+// *** let ***
+// Declaration de la variable "nom" et on lui affecte la valeur "Cazers"
+let nom = "Cazers";
+
+let carton = "la pomme du logo de Apple a déjà été manger.";
+
+// Exercice sur les variables
+let village = "Mtsahara";
+
+
+// *** const ***
+// La valeurs de la variable ne peut pas être modifiée
+const pays = "Mayotte";
+
+// const myHeading = document.querySelector("h1");
+// myHeading.textContent = "Hello world!";
+
+// alert("Le vidéo projecteur d'Acoua");
+// alert(carton);
+
+// La concatenation
+let bienvenu = "Bienvenu " + nom + ", vous etes à " + pays + " et vous habitez à " + village;
+alert(bienvenu);
