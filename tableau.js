c0dabeb7b35fd99d9ce176fa7dbaaff8a2736065
Soihiroudine <59316622+Soihiroudine@users.noreply.github.com>
2024-10-14 13:43:50 +0300

    Travail sur le tableau

---
+++ tableau.js
@@ -0,0 +1,20 @@
+// Manip de tableau
+
+let groupeNom = ["Saïd", "Pomme", "Poire"];
+
+// console.log("le nom afficher : " + groupeNom[0]);
+
+// Mise en place d'un objet : client
+
+let client = {
+    nom : "Cazers",
+    prenom : "Dev",
+    numeroClient : 123,
+    addresse : "15 avenue de l'Arbre",
+    celibataire : false
+};
+
+console.log("Nom : " + client.nom);
+console.log("Prenom : " + client.prenom);
+
+// console.log(client["nom"]);
